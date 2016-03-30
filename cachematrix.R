cb6f811f1e6bc024dd3e4ee794c60bd805249513
Yigit Karabag <ykarabag@yahoo.com>
2016-03-31 00:33:32 +0400

    Programming Assignment 2- Commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A sample exercise showing how expensive computing operations can be reduced by using caching of variables
 
-## Write a short comment describing this function
+## Create functions to set & get the values of matrix as well as inverse of it
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        m<-NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get<-function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function assumes matrix is always invertible and returns the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getinverse()
+        if(!is.null(m)){
+                
+                message("getting cached inverse data")
+                return (m)
+                
+        }
+        
+        data <- x$get()
+        m <- solve(data,...)
+        x$setinverse(m)
+        m
 }
+
+
+## To run the samples use the following code:
+## a <- rbind(c(1,2), c(3,4))
+## b <- makeCacheMatrix(a)
+## print(cacheSolve(b))
+
+
+
